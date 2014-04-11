5181e7a3f6ac0dd78ded8e069ba2b8ef1fb16b5b
balaji-iyer <balajiciyer@gmail.com>
2014-04-10 22:07:05 -0400

    commited new build with file creation

---
+++ GatorGradNet/Common/Utils.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Utils<T>
+    {
+        ///
+        /// Method to write a delimited file, given a generic enumerable.
+        ///
+        /// The type T
+        /// an enumerable of type T
+        /// The destination file info
+        /// The delimiter ("," for example)
+        public static void WriteDelimitedFile(IEnumerable<T> list, FileInfo saveFile, string delimiter)
+        {
+            if (list == null) return;
+            using (StreamWriter sw = saveFile.CreateText())
+            {
+                PropertyInfo[] props = typeof(T).GetProperties();
+                var headerNames = props.Select(x => x.Name);
+                sw.WriteLine(string.Join(delimiter, headerNames.ToArray()));
+                foreach (T item in list)
+                {
+                    T item1 = item; // to prevent access to modified closure
+                    var values = props.Select(x => x.GetValue(item1, null) ?? "") // the null coalescing operator, replace null with ""
+                    .Select(x => x.ToString())
+                    .Select(x => x.Contains(delimiter) ? "\"" + x + "\"" : x); // if a value contains the delimiter, surround with quotes
+                    sw.WriteLine(string.Join(delimiter, values.ToArray()));
+                }
+                sw.Close();
+            }
+        }
+    }
+}
--- GatorGradNet/GatorGradNetConsole/Program.cs
+++ GatorGradNet/GatorGradNetConsole/Program.cs
@@ -1,8 +1,16 @@
-﻿using System;
+﻿using NHibernate;
+using NHibernate.Linq;
+using NHibernateLibrary;
+using NHibernateLibrary;
+using NHibernateLibrary.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Common;
+using System.IO;
+using System.Collections;
 
 namespace GatorGradNet
 {
@@ -10,6 +18,32 @@ namespace GatorGradNet
     {
         static void Main(string[] args)
         {
+
+            //IList<Customer> myList;
+            IList<Address> amyList;
+            ISession session = NHibernateLibrary.Helper.NHibernateHelper.GetCurrentSession();
+
+            ITransaction tx = session.BeginTransaction();
+            ICriteria criteria = session.CreateCriteria(typeof(Customer));
+            //myList= criteria.List<Customer>();
+            //IQuery query = session.CreateQuery("select c from Customer as c");
+            //myList = query.List<Customer>();
+
+            ICriteria addCriteria = session.CreateCriteria(typeof(Address));
+            IQuery aquery = session.CreateQuery("select a from Address as a");
+            amyList = session.Query<Address>().ToList();
+           FileInfo fi = new FileInfo("C:\\temp.txt");
+            // Actually create the file.
+            FileStream fs = fi.Create();
+            fs.Close();
+            
+             // Delete the file.
+            //fi.Delete();
+            Utils<Address>.WriteDelimitedFile(amyList, fi, "\t");
+               // Modify the file as required, and then close the file.
+            fs.Close();
+            //amyList = aquery.List<Address>();
+            Console.WriteLine(amyList.Count());
         }
     }
 }

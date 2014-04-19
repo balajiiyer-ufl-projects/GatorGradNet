fc2d7faabc5b444c814549152ff849cb12a01aa2
rbalaji <r.balaji.610@gmail.com>
2014-04-19 14:20:20 -0400

    Views updated.

--- GatorGradNet/BusinessLayer/RecruitmentStatistics.cs
+++ GatorGradNet/BusinessLayer/RecruitmentStatistics.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace GatorGradNet.BusinessLayer
 {
-    class RecruitmentStatistics
+    public class RecruitmentStatistics
     {
+        public void StatisticsByYear(String CompanyName, String FileLocation)
+        {
+            DataAccessLayer.RecruitmentStatistics.Instance().StatisticsByYear(CompanyName, FileLocation);
+        }
+
+        public void CompanyNameSalary(String Designation, String FileLocation)
+        {
+            DataAccessLayer.RecruitmentStatistics.Instance().CompanyNameSalary(Designation, FileLocation);
+        }
+        public void CompanyTotalHires(String FileLocation)
+        {
+            DataAccessLayer.RecruitmentStatistics.Instance().CompanyTotalHires(FileLocation);
+        }
+        public void DesignationAverageSalary(String FileLocation)
+        {
+            DataAccessLayer.RecruitmentStatistics.Instance().DesignationAverageSalary(FileLocation);
+        }
+        public void ShowDesignationByHires(String FileLocation)
+        {
+            DataAccessLayer.RecruitmentStatistics.Instance().ShowDesignationByHires(FileLocation);
+        }
+        public void ShowMaxRecruitmentEachYear(int year, String FileLocation)
+        {
+            DataAccessLayer.RecruitmentStatistics.Instance().ShowMaxRecruitmentEachYear(year, FileLocation);
+        }
+        public void ShowSalaryVersusHire(String FileLocation)
+        {
+            DataAccessLayer.RecruitmentStatistics.Instance().ShowSalaryVersusHire(FileLocation);
+        }
     }
 }
--- GatorGradNet/BusinessLayer/UserManagement.cs
+++ GatorGradNet/BusinessLayer/UserManagement.cs
@@ -1,5 +1,4 @@
-﻿using Common;
-using NHibernateLibrary.Entities;
+﻿using NHibernateLibrary.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,134 +20,84 @@ namespace GatorGradNet.BusinessLayer
         public ProfileUser Login(String userName, String password)
         {
             NHibernateLibrary.Entities.ProfileUser profileUser = null;
-            try
+            if (userName != null && password != null)
             {
-                if (userName != null && password != null)
-                {
-                    profileUser = new NHibernateLibrary.Entities.ProfileUser();
-                    profileUser.Username = userName;
-                    profileUser.Password = password;
-                    profileUser = DataAccessLayer.UserManagement.Instance().Login(profileUser);
-                }
-                //return new DataAccessLayer.UserManagement().Login(profileUser);
-                return profileUser;
+                profileUser = new NHibernateLibrary.Entities.ProfileUser();
+                profileUser.Username = userName;
+                profileUser.Password = password;
+                profileUser = DataAccessLayer.UserManagement.Instance().Login(profileUser);
             }
-            catch (Exception exception)
-            {
-                throw new BusinessLayerException(exception.Message, exception.InnerException);
-            }
+            //return new DataAccessLayer.UserManagement().Login(profileUser);
+            return profileUser;
         }
 
         public bool UserExistsOrNot(String userName)
         {
-
             bool userExists = false;
             NHibernateLibrary.Entities.ProfileUser profileUser = null;
-            try
+            if (userName != null)
             {
-                if (userName != null)
-                {
-                    profileUser = new NHibernateLibrary.Entities.ProfileUser();
-                    profileUser.Username = userName;
-                    userExists = DataAccessLayer.UserManagement.Instance().CheckIfUserExists(profileUser);
-                }
-                return userExists;
+                profileUser = new NHibernateLibrary.Entities.ProfileUser();
+                profileUser.Username = userName;
+                userExists = DataAccessLayer.UserManagement.Instance().CheckIfUserExists(profileUser);
             }
-            catch (Exception exception)
-            {
-                throw new BusinessLayerException(exception.Message, exception.InnerException);
-            }
+            return userExists;
         }
         public bool RegisterUser(String Username, String Password, String Email)
         {
             NHibernateLibrary.Entities.ProfileUser PUser = new NHibernateLibrary.Entities.ProfileUser();
-            try
-            {
-                PUser.Username = Username;
-                PUser.Password = Password;
-                PUser.Email = Email;
-                PUser.PrevEducation = "";
-                PUser.PrevLocation = "";
-                PUser.PrevWork = "";
-                PUser.CurrentWork = "";
-                //PUser.
+            PUser.Username = Username;
+            PUser.Password = Password;
+            PUser.Email = Email;
+            PUser.PrevEducation = "";
+            PUser.PrevLocation = "";
+            PUser.PrevWork = "";
+            PUser.CurrentWork = "";
+            //PUser.
 
-                return DataAccessLayer.UserManagement.Instance().RegisterUser(PUser);
-            }
-            catch (Exception exception)
-            {
-                throw new BusinessLayerException(exception.Message, exception.InnerException);
-            }
+            return DataAccessLayer.UserManagement.Instance().RegisterUser(PUser);
         }
-        public bool UpdateUserProfile(int UserID, String Username, String Password, String Email, String PrevEducation, String PrevWork, String PrevLocation, String CurrentWork)
+        public bool UpdateUserProfile(String Username, String FirstName, String LastName, String Password, String Email, String PrevEducation, String PrevWork, String PrevLocation, String CurrentWork, float GPA, String DreamCompany,String Branch)
         {
-            NHibernateLibrary.Entities.ProfileUser PUser = new NHibernateLibrary.Entities.ProfileUser();
-            try
-            {
-                //PUser. = UserID;
-                PUser.Username = Username;
-                PUser.Password = Password;
-                PUser.Email = Email;
-                PUser.PrevEducation = PrevEducation;
-                PUser.PrevLocation = PrevLocation;
-                PUser.PrevWork = PrevWork;
-                return DataAccessLayer.UserManagement.Instance().RegisterUser(PUser);
-            }
-            catch (Exception exception)
-            {
-                throw new BusinessLayerException(exception.Message, exception.InnerException);
-            }
+
+            ProfileUser user = BusinessLayer.UserManagement.Instance().GetUserProfile(Username);
+            if(!String.IsNullOrEmpty(FirstName))user.FirstName = FirstName;
+            if (!String.IsNullOrEmpty(LastName)) user.LastName = LastName;
+            if (!String.IsNullOrEmpty(Password)) user.Password = Password;
+            if (!String.IsNullOrEmpty(Email)) user.Email = Email;
+            if (!String.IsNullOrEmpty(PrevEducation)) user.PrevEducation = PrevEducation;
+            if (!String.IsNullOrEmpty(PrevWork)) user.PrevWork = PrevWork;
+            if (!String.IsNullOrEmpty(PrevLocation)) user.PrevLocation = PrevLocation;
+            if (!String.IsNullOrEmpty(CurrentWork)) user.CurrentWork = CurrentWork;
+            if (!float.IsNaN(GPA)) user.GPA = GPA;
+            if (!String.IsNullOrEmpty(DreamCompany)) user.DreamCompany = DreamCompany;
+            if (!String.IsNullOrEmpty(Branch)) user.Branch = Branch;
+            return DataAccessLayer.UserManagement.Instance().UpdateUserProfile(user);
         }
         public NHibernateLibrary.Entities.ProfileUser GetUserProfile(String Username)
         {
             NHibernateLibrary.Entities.ProfileUser PUser = new NHibernateLibrary.Entities.ProfileUser();
-            try
-            {
-                PUser.Username = Username;
-                return DataAccessLayer.UserManagement.Instance().GetUserProfile(PUser);
-            }
-            catch (Exception exception)
-            {
-                throw new BusinessLayerException(exception.Message, exception.InnerException);
-            }
+            PUser.Username = Username;
+            return DataAccessLayer.UserManagement.Instance().GetUserProfile(PUser);
         }
         public IList<ProfileUser> GetGatorsByLocation(String Location)
         {
             //NHibernateLibrary.Entities.ProfileUser PUser = new NHibernateLibrary.Entities.ProfileUser();
             //PUser.Username = Username;
-            try
-            {
-                return DataAccessLayer.UserManagement.Instance().GetGators(DataAccessLayer.UserManagement.Criterion.Location, Location);
-            }
-            catch (Exception exception)
-            {
-                throw new BusinessLayerException(exception.Message, exception.InnerException);
-            }
-        }
 
+            return DataAccessLayer.UserManagement.Instance().GetGators(DataAccessLayer.UserManagement.Criterion.Location, Location);
+        }
         public IList<ProfileUser> GetGatorsByPreviousEmployment(String Employment)
         {
             //NHibernateLibrary.Entities.ProfileUser PUser = new NHibernateLibrary.Entities.ProfileUser();
             //PUser.Username = Username;
-            try
-            {
-                return DataAccessLayer.UserManagement.Instance().GetGators(DataAccessLayer.UserManagement.Criterion.Work, Employment);
-            }
-            catch (Exception exception)
-            {
-                throw new BusinessLayerException(exception.Message, exception.InnerException);
-            }
+        
+            return DataAccessLayer.UserManagement.Instance().GetGators(DataAccessLayer.UserManagement.Criterion.Work, Employment);
         }
         public IList<ProfileUser> GetGatorsByPreviousSchool(String Education)
         {
-            try
-            {
-                return DataAccessLayer.UserManagement.Instance().GetGators(DataAccessLayer.UserManagement.Criterion.Education, Education);
-            }
-            catch (Exception exception)
-            {
-                throw new BusinessLayerException(exception.Message, exception.InnerException);
-            }
+        
+            return DataAccessLayer.UserManagement.Instance().GetGators(DataAccessLayer.UserManagement.Criterion.Education, Education);
         }
     }
 }

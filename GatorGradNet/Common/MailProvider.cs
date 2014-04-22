d39a4141af947818697882cedd3f6d303d312e20
balaji-iyer <balajiciyer@gmail.com>
2014-04-22 01:52:16 -0400

    Mapping file for Company Salary Stats

--- GatorGradNet/Common/MailProvider.cs
+++ GatorGradNet/Common/MailProvider.cs
@@ -75,6 +75,33 @@ namespace Common
         }
 
 
+        public void MailSend(string ToAddress, string subject,string )
+        {
+            using (MailMessage mailMessage = new MailMessage())
+            {
+
+                mailMessage.From = new MailAddress(Constants.FROM_ADDRESS);
+                mailMessage.Subject = subject;
+
+                //mailMessage.Body = "Hello " + firstName + "! <p> Your new password is " + verificationcode + " </p><br>Sincerely,</br><br>The GatorGradNet team</br>";
+                //mailMessage.Body = "http://server address/Verificationpage.aspx?code=" + verificationcode;
+                mailMessage.IsBodyHtml = true;
+                mailMessage.To.Add(new MailAddress(ToAddress));
+
+
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = Constants.HOST;
+                smtp.EnableSsl = true;
+                System.Net.NetworkCredential NetworkCred = new System.Net.NetworkCredential();
+                NetworkCred.UserName = mailMessage.From.Address;
+                NetworkCred.Password = Constants.PASSWORD;
+                smtp.UseDefaultCredentials = true;
+                smtp.Credentials = NetworkCred;
+                smtp.Port = Constants.PORT;
+                smtp.Send(mailMessage);
+            }
+        }
+
 
         #endregion
     }
